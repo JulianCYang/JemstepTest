aaf66fcaa77a748e898921b3dba41e5cdc8c8da6
Student <julianyang888@gmail.comgit>
2019-07-03 17:09:09 +0200

    Inconsistent prints

--- composetest/app.py
+++ composetest/app.py
@@ -3,7 +3,7 @@ import time
 import redis
 from flask import Flask
 
-app = Flask(__name__)
+app = Flask(__name__) #creates the application object as an instance of Flask (the "___name__" variable is a Python predefined variable which is set to the name of the module in which it is used)
 cache = redis.Redis(host='redis', port=6379)
 print("Started app...")
 
@@ -12,7 +12,8 @@ def get_hit_count():
     while True:
         try:
             redisValue = cache.incr('hits')
-            print("got cache:" + redisValue)
+            print("Got cache :"+str(redisValue))
+            #print(redisValue)
             return redisValue
         except redis.exceptions.ConnectionError as exc:
             if retries == 0:
@@ -21,8 +22,9 @@ def get_hit_count():
             time.sleep(0.5)
 
 
-@app.route('/')
+@app.route('/morne') #Routes are the different "URLs" that the application implements (view functions)
 def hello():
     count = get_hit_count()
-    print("Counting: "+ count)
+    print ("Counting :"+ str(count))
     return 'Hello from Docker! I have been seen {} times.\n'.format(count)
+    #return 'Hello World'
