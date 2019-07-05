0cf191ec4035255532ac0cff03999c7f209ae0fc
Student <julianyang888@gmail.comgit>
2019-07-05 12:03:13 +0200

    Remove useless comments

--- weatherChecker/app2.py
+++ weatherChecker/app2.py
@@ -1,13 +1,16 @@
 #!/usr/bin/python3
+#https://medium.com/@aakankshaws/using-beautifulsoup-requests-to-scrape-weather-data-9c6e9d317800
 import time
 import redis
 from requests_html import HTMLSession
 import requests
 from bs4 import BeautifulSoup
 
+def insertValue(key,value):
+
 
 cache = redis.Redis(host='172.17.0.2', port=6379)
-cache.set('poo', 'bar')
+cache.insertValue('poo', 'bar')
 value = cache.get('poo')
 print(value)
 
@@ -15,26 +18,7 @@ session = HTMLSession()
 r = session.get('https://www.accuweather.com/en/za/johannesburg/305448/weather-forecast/305448')
 page = requests.get("https://weather.com/en-IN/weather/tenday/l/INKA0344:1:IN")
 page = requests.get("https://www.accuweather.com/en/za/johannesburg/305448/weather-forecast/305448")
-soup=BeautifulSoup(page.content,"html.parser")
-#print(r.html.absolute_links)
 about = r.html.find('#about', first=True)
 
-all=soup.find("div",{"class":"locations-title ten-day-page-title"}).find("h1").text
-table=soup.find_all("table",{"class":"twc-table"})
-l=[]
-for items in table:
- for i in range(len(items.find_all("tr"))-1):
-  d = {}  
-  d["day"]=items.find_all("span",{"class":"date-time"})[i].text
-  d["date"]=items.find_all("span",{"class":"day-detail"})[i].text
-  d["desc"]=items.find_all("td",{"class":"description"})[i].text 
-  d["temp"]=items.find_all("td",{"class":"temp"})[i].text 
-  d["precip"]=items.find_all("td",{"class":"precip"})[i].text
-  d["wind"]=items.find_all("td",{"class":"wind"})[i].text  
-  d["humidity"]=items.find_all("td",{"class":"humidity"})[i].text 
-  l.append(d)
-
 print ("Hello World!")
-for i in range(len(l)): 
-    print (l[i])
 print (r.html.search('Python is a {} language')[0])
